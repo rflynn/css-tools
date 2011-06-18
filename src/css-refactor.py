a1167d3ef5b9d49045601b2caa01e67a689baba5
pizza <parseerror@gmail.com>
2011-06-17 23:45:15 -0400

    Wrong branch
    This reverts commit c94b5a0463ce4f19bf2c6068e87458134921121a.

--- src/css-refactor.py
+++ src/css-refactor.py
@@ -34,44 +34,9 @@ else:
 	filename = '-'
 	contents = sys.stdin.read()
 
-cssparse.Format.canonical()
-
 doc = cssparse.CSSDoc.parse(contents)
 ref = cssrefactor.CSSRefactor(doc)
 
-import itertools
-
-# list of all overlapping decls subsets
-overlap = (set(x.decls.decl) & set(y.decls.decl)
-		for x,y in itertools.combinations(ref.rules, 2))
-overlap2 = filter(None, overlap)
-print overlap2
-od = dict.fromkeys(map(tuple, overlap2), {})
-
-# for each unique overlapping subset, build a set of all
-# decls that share it
-for r in ref.rules:
-	rs = set(r.decls.decl)
-	for k, v in od.items():
-		ks = set(k)
-		if len(ks & rs) == len(ks):
-			v[r] = 1
-print od
-
-# calculate the sum total length of the selectors of the overlapping decls
-
-# sort the overlapping subsets by total size, i.e.
-# figure out which ones will save the most space if condensed
-
-# if we have 1 or more overlapping subsets, and the best one will save us
-# space, use it. pull the subsets out of each decl and build a new rule from
-# their selectors. save this for later.
-
-# the removal of these subsets of 2+ selectors could potentially effect
-# every other overlapping subset, so do everything over again until we run out
-# of subset operations that save space.
-
-# now merge the remaining doc rules with the factored-out ones into a coherent document
-
-#print ref.format()
+cssparse.Format.canonical()
+print ref.format()
 
